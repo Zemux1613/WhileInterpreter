f2d5bd038a17e79312dd60738c5dd0c49265f5a7
Tom L <devzemux1613@gmail.com>
2023-10-07 00:17:56 +0200

    feat: add while example and prepare for parsing

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+import sys
+from Lexer import Lexer
+
+from utils.FileUtil import *
+
 if __name__ == "__main__":
-    print("hello world")
+    path = input("Welche Datei soll interpretiert werden? ")
+    if not path:
+        path = "examples/add.while"
+
+    if not path.endswith(".while"):
+        print(f"Unter '{path}' ist keine Quellcode Datei.")
+        sys.exit(-1)
+
+    source_code = FileUtil.readFile(FileUtil(), path)
+    instructions = Lexer(source_code).lex()
+    for instruction in instructions:
+        print(str(instruction))
