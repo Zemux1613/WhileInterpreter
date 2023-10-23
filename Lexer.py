aa1287e8d56d9afa4c3b4c1980648bc5635171e3
Tom L <devzemux1613@gmail.com>
2023-10-23 09:41:06 +0200

    fix: tokenizing

--- Lexer.py
+++ Lexer.py
@@ -1,14 +1,11 @@
 import re
-import sys
-from enum import Enum
-
 
 class Lexer:
 
     def __init__(self, content):
         self.content = content
         self.lines = content.split("\n")
-        self.instructions = []
+        self.tokens = []
 
     def lex(self):
         for i in range(0, len(self.lines), 1):
@@ -27,65 +24,32 @@ class Lexer:
             if ";" in line:
                 line = line[0:line.index(";")]
 
-            token = None
+            for word in re.sub(r'(x(\d+))', r'x \2', line).split():
+                tokenized_word = word
 
-            for t in Token.all():
-                if re.match(t.get_identifier(), line.strip()):
-                    token = t
-                    break
+                tokenized_word = re.sub(r"([0-9])+", "num", tokenized_word)
+                tokenized_word = re.sub(r"(\+|-)+", "operator", tokenized_word)
 
-            if token is None:
-                print(f"Invalid line: '{line}'")
-                sys.exit(i)
+                token = Token(tokenized_word, word)
 
-            self.instructions.append(Instruction(i + 1, token))
+                self.tokens.append(token)
 
-        return self.instructions
+        return self.tokens
 
-    def get_instructions(self):
-        return self.instructions
+    def get_tokens(self):
+        return self.tokens
 
 
-class Token(Enum):
-    ASSIGNMENT = r"([a-zA-Z]+[0-9]*\s*:=\s*[a-zA-Z]+[0-9]*\s*(\+|-)\s*[0-9]+)", False
-    PRINT = r"(print\s*[a-zA-Z]+[0-9]*)", False
-    WHILE = r"(while)", True
-    END = r"(end)", False
-
-    def __init__(self, identifier, multi_line):
+class Token:
+    def __init__(self, identifier, value):
         self.identifier = identifier
-        self.multi_line = multi_line
+        self.value = value
 
     def get_identifier(self):
         return self.identifier
 
-    def get_multi_line(self):
-        return self.multi_line
-
-    @classmethod
-    def all(cls):
-        return [getattr(cls, attr) for attr in cls.__members__]
-
-
-class Instruction:
-    def __init__(self, line, token):
-        self.line = line
-        self.token = token
-
-    def get_token(self):
-        return self.token
-
-    def get_line(self):
-        return self.line
+    def get_value(self):
+        return self.value
 
     def __str__(self):
-        return f'{self.line}: {self.token.name}'
-
-
-class MultilineInstruction(Instruction):
-    def __init__(self, line, token, instructions):
-        super().__init__(line, token)
-        self.instructions = instructions
-
-    def get_instructions(self):
-        return self.instructions
+        return f"{self.identifier}: {self.value}"
