291dc4af6421b841333e2fdc7d98d234a580c306
Tom L <devzemux1613@gmail.com>
2023-11-18 19:27:23 +0100

    feat: add print statement

--- 2.0/Parser.py
+++ 2.0/Parser.py
@@ -8,10 +8,9 @@ class Parser:
         self.position = 0
 
     def consume(self, pattern):
-        print(
-            f"trying to consume token={self.tokens[self.position]}, patter='{pattern}', position={self.position}, tokens={self.tokens[self.position:]}")
+        # print(f"trying to consume token={self.tokens[self.position]}, patter='{pattern}', position={self.position}, tokens={self.tokens[self.position:]}")
         if re.match(pattern, self.tokens[self.position]):
-            print(f"consume {pattern}")
+        #    print(f"consume {pattern}")
             self.position += 1
         else:
             raise Exception(f"invalid syntax, token {self.tokens[self.position]} didn't macht with '{pattern}'")
@@ -20,13 +19,17 @@ class Parser:
         # first statement is required
         first_statement = self.statement()
 
-        statements = [first_statement]
+        # indexing statements
+        index = 0
+
+        statements = {index: first_statement}
 
         # other statements are optional
         while ";" in self.tokens[self.position:]:
-            self.consume("(;)")
+            self.consume(r"(;)")
             statement = self.statement()
-            statements.append(statement)
+            index += 1
+            statements[index] = statement
 
         return statements
 
@@ -38,40 +41,50 @@ class Parser:
         except Exception as ex:
             self.position = start
             try:
-                statement = self.loop()
+                statement = self.print()
             except Exception as ex1:
                 self.position = start
+                try:
+                    statement = self.loop()
+                except Exception as ex2:
+                    self.position = start
         return statement
+
+    def print(self):
+        start = self.position
+        self.consume("(print)")
+        self.variable()
+        return ' '.join(self.tokens[start:self.position])
 
     def assignment(self):
         start = self.position
         self.variable()
-        self.consume("(=)")
+        self.consume(r"(=)")
         self.variable()
         self.operator()
         self.number()
         return ' '.join(self.tokens[start:self.position])
 
     def number(self):
-        self.consume("([0-9]+)")
+        self.consume(r"([0-9]+)")
 
     def operator(self):
-        self.consume("(\+|-)")
+        self.consume(r"(\+|-)")
 
     def variable(self):
-        self.consume("([a-z]+)")
+        self.consume(r"([a-z]+)")
 
     def parse(self):
         return self.programm()
 
     def loop(self):
         start = self.position
-        self.consume("(while)")
+        self.consume(r"(while)")
         self.variable()
-        self.consume("(!)")
-        self.consume("(=)")
+        self.consume(r"(!)")
+        self.consume(r"(=)")
         self.number()
-        self.consume("(do)")
+        self.consume(r"(do)")
         self.programm()
-        self.consume("(end)")
+        self.consume(r"(end)")
         return ' '.join(self.tokens[start:self.position])
