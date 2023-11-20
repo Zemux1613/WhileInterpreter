2a15aeac87c05d0a4bfeaa9e8e9e3092a7e9b006
Tom L <devzemux1613@gmail.com>
2023-11-20 08:56:34 +0100

    feat: change default

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from utils.FileUtil import *
 if __name__ == "__main__":
     path = input("Welche Datei soll interpretiert werden? ")
     if not path:
-        path = "examples/assign.while"
+        path = "examples/add.while"
 
     if not path.endswith(".while"):
         print(f"Unter '{path}' ist keine Quellcode Datei.")
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
     source_code = FileUtil.read_file(FileUtil(), path)
     instructions = Lexer(source_code).lex()
-    # for instruction in instructions:
-    #print(str(instruction))
+    #for instruction in instructions:
+    #    print(str(instruction))
 
     program = Parser(instructions).parse()
