961e231a9a45e05fbe247a9ad2407adefdfb2fd2
Tom L <devzemux1613@gmail.com>
2023-11-20 09:08:31 +0100

    feat: add sample multiplication

--- 2.0/main.py
+++ 2.0/main.py
@@ -8,7 +8,7 @@ from Interpreter import Interpreter
 if __name__ == "__main__":
     path = input("Welche Datei soll interpretiert werden? ")
     if not path:
-        path = "../examples/add.while"
+        path = "../examples/mul.while"
 
     if not path.endswith(".while"):
         print(f"Unter '{path}' ist keine Quellcode Datei.")
